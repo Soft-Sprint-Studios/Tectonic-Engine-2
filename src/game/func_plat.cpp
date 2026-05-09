727351bab5f7f23a52fbaa275ce37cddc75a36d0
Jdjd Gaming <80889633+valina354@users.noreply.github.com>
2026-05-09 21:48:22 +0300

    new entities

---
+++ src/game/func_plat.cpp
@@ -0,0 +1,179 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025-2026 Soft Sprint Studios
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "entities.h"
+#include "physics.h"
+#include "timing.h"
+#include <btBulletDynamicsCommon.h>
+
+class FuncPlat : public Entity
+{
+public:
+    enum State
+    {
+        Bottom,
+        Up,
+        Top,
+        Down
+    };
+
+    void Spawn(const std::unordered_map<std::string, std::string>& keyvalues) override
+    {
+        Entity::Spawn(keyvalues);
+
+        m_speed = GetFloat("speed", 50.0f) * BSP::MAPSCALE;
+        m_wait = GetFloat("wait", 3.0f);
+        
+        float height = GetFloat("height", 0.0f);
+
+        m_startOrigin = m_origin;
+
+        m_targetOrigin = m_startOrigin + glm::vec3(0.0f, height * BSP::MAPSCALE, 0.0f);
+        m_state = Bottom;
+
+        UpdatePhysicsState();
+    }
+
+    void OnSave() override
+    {
+        Entity::OnSave();
+        AddSaveField(DATA_FIELD(FuncPlat, m_state, FieldType::Int32));
+        AddSaveField(DATA_FIELD(FuncPlat, m_startOrigin, FieldType::Vec3));
+        AddSaveField(DATA_FIELD(FuncPlat, m_targetOrigin, FieldType::Vec3));
+        AddSaveField(DATA_FIELD(FuncPlat, m_nextActionTime, FieldType::Float));
+    }
+
+    void Think(float deltaTime) override
+    {
+        if (m_state == Up)
+        {
+            MoveTo(m_targetOrigin, deltaTime, Top);
+        }
+        else if (m_state == Down)
+        {
+            MoveTo(m_startOrigin, deltaTime, Bottom);
+        }
+        else if (m_state == Top && m_wait >= 0.0f)
+        {
+            if (Time::TotalTime() >= m_nextActionTime)
+            {
+                m_state = Down;
+            }
+        }
+    }
+
+    void Touch(Entity* other) override
+    {
+        if (HasSpawnFlag(1))
+        {
+            return;
+        }
+
+        if (m_targetName.empty() && m_state == Bottom && other && other->IsPlayer())
+        {
+            Trigger();
+        }
+    }
+
+    void AcceptInput(const std::string& inputName, const std::string& parameter) override
+    {
+        if (inputName == "Toggle")
+        {
+            Trigger();
+        }
+        else if (inputName == "GoUp")
+        {
+            if (m_state == Bottom || m_state == Down)
+            {
+                m_state = Up;
+            }
+        }
+        else if (inputName == "GoDown")
+        {
+            if (m_state == Top || m_state == Up)
+            {
+                m_state = Down;
+            }
+        }
+    }
+
+    bool IsRenderable() const override
+    {
+        return true;
+    }
+
+private:
+    void Trigger()
+    {
+        if (m_state == Bottom)
+        {
+            m_state = Up;
+        }
+        else if (m_state == Top && m_wait < 0.0f)
+        {
+            m_state = Down;
+        }
+    }
+
+    void MoveTo(const glm::vec3& target, float dt, State arriveState)
+    {
+        float dist = glm::distance(m_origin, target);
+        float step = m_speed * dt;
+
+        if (step >= dist)
+        {
+            m_origin = target;
+            m_state = arriveState;
+
+            if (m_state == Top)
+            {
+                m_nextActionTime = (float)Time::TotalTime() + m_wait;
+            }
+        }
+        else
+        {
+            m_origin += glm::normalize(target - m_origin) * step;
+        }
+
+        UpdatePhysicsTransform();
+    }
+
+    void UpdatePhysicsTransform()
+    {
+        if (m_physObject)
+        {
+            btTransform trans = m_physObject->getWorldTransform();
+            trans.setOrigin({ m_origin.x, m_origin.y, m_origin.z });
+            m_physObject->setWorldTransform(trans);
+        }
+    }
+
+    State m_state = Bottom;
+    glm::vec3 m_startOrigin;
+    glm::vec3 m_targetOrigin;
+    float m_speed = 0.0f;
+    float m_wait = 0.0f;
+    float m_nextActionTime = 0.0f;
+};
+
+LINK_ENTITY_TO_CLASS("func_plat", FuncPlat)
---
+++ src/game/trigger_chance.cpp
@@ -0,0 +1,122 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025-2026 Soft Sprint Studios
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "entities.h"
+#include "timing.h"
+#include <random>
+
+class TriggerChance : public Entity
+{
+public:
+    void Spawn(const std::unordered_map<std::string, std::string>& keyvalues) override
+    {
+        Entity::Spawn(keyvalues);
+        m_chance = GetFloat("chance", 50.0f);
+        m_wait = GetFloat("wait", 1.0f);
+        m_disabled = HasSpawnFlag(1);
+        m_nextFireTime = 0.0f;
+    }
+
+    void OnSave() override
+    {
+        Entity::OnSave();
+        AddSaveField(DATA_FIELD(TriggerChance, m_disabled, FieldType::Bool));
+        AddSaveField(DATA_FIELD(TriggerChance, m_nextFireTime, FieldType::Float));
+    }
+
+    void Touch(Entity* other) override
+    {
+        if (m_disabled)
+        {
+            return;
+        }
+
+        if (other && other->IsPlayer())
+        {
+            if (Time::TotalTime() >= m_nextFireTime)
+            {
+                CalculateChance();
+
+                if (m_wait < 0.0f)
+                {
+                    m_disabled = true;
+                }
+                else
+                {
+                    m_nextFireTime = (float)Time::TotalTime() + m_wait;
+                }
+            }
+        }
+    }
+
+    void AcceptInput(const std::string& input, const std::string& param) override
+    {
+        if (input == "Enable")
+        {
+            m_disabled = false;
+        }
+        else if (input == "Disable")
+        {
+            m_disabled = true;
+        }
+        else if (input == "Toggle")
+        {
+            m_disabled = !m_disabled;
+        }
+        else if (input == "Trigger")
+        {
+            if (!m_disabled)
+            {
+                CalculateChance();
+            }
+        }
+    }
+
+    bool IsCollidable() const override
+    {
+        return false;
+    }
+
+private:
+    void CalculateChance()
+    {
+        static std::mt19937 gen(std::random_device{}());
+        std::uniform_real_distribution<float> dist(0.0f, 100.0f);
+
+        if (dist(gen) <= m_chance)
+        {
+            FireOutput("OnPass");
+        }
+        else
+        {
+            FireOutput("OnFail");
+        }
+    }
+
+    float m_chance = 50.0f;
+    float m_wait = 1.0f;
+    float m_nextFireTime = 0.0f;
+    bool m_disabled = false;
+};
+
+LINK_ENTITY_TO_CLASS("trigger_chance", TriggerChance)
