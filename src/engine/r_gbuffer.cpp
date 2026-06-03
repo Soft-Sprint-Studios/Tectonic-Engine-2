d6cc8ec645ed37e0221778397bbddcf1ddb1208f
Jdjd Gaming <80889633+valina354@users.noreply.github.com>
2026-06-03 19:42:09 +0300

    revert normal gbuffer change tdue to not supported on some gpus

--- src/engine/r_gbuffer.cpp
+++ src/engine/r_gbuffer.cpp
@@ -50,7 +50,7 @@ bool R_GBuffer::Init(int width, int height)
     // Normal
     glGenTextures(1, &m_normalTex);
     glBindTexture(GL_TEXTURE_2D, m_normalTex);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16_SNORM, width, height, 0, GL_RGBA, GL_SHORT, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, width, height, 0, GL_RGBA, GL_FLOAT, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, m_normalTex, 0);
